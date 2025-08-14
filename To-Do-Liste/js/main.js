cfb5dfe46613e80890bf13b3a7b343514de6fe00
David Schuchert <ds@dbe.academy>
2025-08-14 08:06:34 +0200

    Todo korrigiert

--- To-Do-Liste/js/main.js
+++ To-Do-Liste/js/main.js
@@ -5,12 +5,16 @@ let list = document.querySelector("#list");
 
 //Event
 
+
+//Überprüfung fehlt wenn ich nichts eingebe -> er fügt was leeres ein
 document.addEventListener("DOMContentLoaded", ()=> {
     button.addEventListener("click", function() {
-        let newTask = document.createElement("li");
+        let newTask = document.createElement("li"); //ist korrekt so, würde hier aber innerhtml nutzen
         newTask.textContent = input.value;
         list.appendChild(newTask);
         input.value = "";
     })
 });
+
+//aber alles super!!
 
