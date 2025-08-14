da0c1542ed16ea2fd4693390e97aba87724f063a
David Schuchert <ds@dbe.academy>
2025-08-14 08:04:57 +0200

    Zufallszahl korrigiert

--- Zufallszahl_generator/js/main.js
+++ Zufallszahl_generator/js/main.js
@@ -5,12 +5,12 @@ let button = document.querySelector("#btn");
 let result = document.querySelector("#result");
 
 //function
-function randomNumber () {
-    return Math.floor(Math.random() * 100);
+function randomNumber () { //hier in der () klammer erwartest du kein Parameter
+    return Math.floor(Math.random() * 100); //Hier setzt du die 100 als grenze - korrekt!
 };
 
 //generate
 
 button.addEventListener("click", function() {
-    result.innerHTML = `Deine Zahl: ${randomNumber(100)}`;
+    result.innerHTML = `Deine Zahl: ${randomNumber(100)}`; //Hier in der klammer übergibst du ein parameter erwartest oben aber keinen, kannst du also weglassen einfach ${randomNumber()}
 });
